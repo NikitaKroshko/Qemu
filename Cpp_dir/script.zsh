a0abb1843b27de79e3b00120360d11b95f53eda1
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-09-21 16:07:19 +0100

    change it into c++

--- Cpp_dir/script.zsh
+++ Cpp_dir/script.zsh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <sourcefile.c>"
+  echo "Usage: $0 <sourcefile.cpp>"
   exit 1
 fi
 
 src_file="$1"
 base_name="${src_file:r}"
 
-gcc "$src_file" -o "$base_name.out"
+g++-15 "$src_file" -o "$base_name.out"
 if [ $? -ne 0 ]; then
   echo "Compilation failed."
   exit 1
